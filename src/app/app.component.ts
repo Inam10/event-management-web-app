53a6caa754e29954be2e37fc7fe9f21884a68e63
Mukut Prasad <mukutprasad1992@gmail.com>
2023-11-21 18:03:34 +0530

    dashboard added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'EventfulEdge Solutions';
+export class AppComponent implements OnInit {
+    shouldShowNavbar: boolean = true;
 
- 
+    constructor(private router: Router) {}
+
+    ngOnInit() {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                // Adjust the condition based on your routing logic
+                this.shouldShowNavbar = !event.url.includes('/dashboard');
+            }
+        });
+    }
 }
