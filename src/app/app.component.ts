d0b084e396c2b37857cad905d4723148db92a9b3
Mukut Prasad <mukutprasad1992@gmail.com>
2023-11-21 14:29:43 +0530

    fix minor issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'event-management-web-app';
+  title = 'EventfulEdge Solutions';
 
  
 }
