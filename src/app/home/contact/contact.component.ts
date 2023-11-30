9ec7f497b5ad3ec08de3360c7fc8a4f69acc5f41
Inam10 <inamm94@gmail.com>
2023-11-30 15:17:17 +0530

    update

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
-
+  
 }
+
+
